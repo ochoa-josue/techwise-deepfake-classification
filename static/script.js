0e15cae814f478f5a28acfb909db56c20219c9d4
elenachau <elenaxcha@gmail.com>
2024-03-10 12:50:24 -0700

    change fetch link for gcloud

--- static/script.js
+++ static/script.js
@@ -1,34 +1,36 @@
 function uploadImage() {
-  const input = document.getElementById('imageInput');
-  const resultDiv = document.getElementById('result');
-  
-  if (input.files && input.files.length > 0) {
-      const formData = new FormData();
-      for (const file of input.files) {
-          formData.append('image', file, file.name);
-      }
+  const input = document.getElementById("imageInput");
+  const resultDiv = document.getElementById("result");
 
-      fetch('http://127.0.0.1:8000/upload', {
-          method: 'POST',
-          body: formData,
+  if (input.files && input.files.length > 0) {
+    const formData = new FormData();
+    for (const file of input.files) {
+      formData.append("image", file, file.name);
+    }
+
+    fetch("https://deepfake-classifier-qa5nhs5lga-uw.a.run.app/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        resultDiv.innerHTML = data
+          .map((item) => {
+            if (item.error) {
+              return `<p style="color: red;">${item.filename}: Error - ${item.error}</p>`;
+            } else {
+              return `<p style="color: green;">${item.filename}: Is Deepfake - ${item.is_deepfake}</p>`;
+            }
+          })
+          .join("");
       })
-      .then(response => response.json())
-      .then(data => {
-          console.log(data);
-          resultDiv.innerHTML = data.map(item => {
-              if (item.error) {
-                  return `<p style="color: red;">${item.filename}: Error - ${item.error}</p>`;
-              } else {
-                  return `<p style="color: green;">${item.filename}: Is Deepfake - ${item.is_deepfake}</p>`;
-              }
-          }).join('');
-      })
-      .catch(error => {
-          console.error(error);
-          resultDiv.innerHTML = `<p style="color: red;">Error processing the images.</p>`;
+      .catch((error) => {
+        console.error(error);
+        resultDiv.innerHTML = `<p style="color: red;">Error processing the images.</p>`;
       });
   } else {
-      resultDiv.innerHTML = `<p style="color: red;">Please select at least one image file.</p>`;
+    resultDiv.innerHTML = `<p style="color: red;">Please select at least one image file.</p>`;
   }
 }
 
